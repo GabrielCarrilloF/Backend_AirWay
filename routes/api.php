7d5f724c4b5abfcf9e87ed72a692d1f0fcae2ab2
GabrielCarrilloF <gabocarrillo.deve@gmail.com>
2024-11-04 14:30:15 -0500

    SCRUM-26 add Routes in api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/companies', function () {
+    return 'Lista de companies';
+});
+
+Route::get('/companies/{id}', function () {
+    return 'una sola companie';
+});
+
+Route::post('/companies', function () {
+    return 'crear';
+});
+
+Route::put('/companies/{id}', function () {
+    return 'actualizar';
+});
+
+Route::delete('/companies/{id}', function () {
+    return 'eliminar';
+});
